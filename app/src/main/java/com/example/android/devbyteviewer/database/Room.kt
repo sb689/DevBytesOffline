7f075d6fd8d44f200df82d9c4cbe2349b0b53d17
Sujata Baral <sujata689@gmail.com>
2021-01-02 20:18:54 -0600

    repository implement for offline caching of network data

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -36,20 +36,21 @@ interface VideoDao {
 
 @Database(entities = [DatabaseVideo::class], version = 1)
 abstract class VideosDatabase : RoomDatabase() {
-
     abstract val videoDao: VideoDao
-    private lateinit var INSTANCE: VideosDatabase
+}
 
-    fun getDatabase(context: Context): VideosDatabase {
+private lateinit var INSTANCE: VideosDatabase
 
-        synchronized(VideosDatabase::class.java) {
-            if (!::INSTANCE.isInitialized) {
-                INSTANCE = Room.databaseBuilder(context.applicationContext,
-                        VideosDatabase::class.java,
-                        "videos").build()
-            }
+fun getDatabase(context: Context): VideosDatabase {
+
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos").build()
         }
-        return INSTANCE
     }
+    return INSTANCE
+
 
 }
